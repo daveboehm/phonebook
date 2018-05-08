7fbc7821f75c2a4946f0c3107bb14b4808c3f0ba
Dave Boehm <davidnboehm@gmail.com>
2018-05-08 18:30:08 -0500

    basic routes set up

---
+++ src/containers/Create/Create.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Create extends Component {
+    constructor( props ) {
+        super(props)
+    }
+
+    render() {
+        return ( <div>Create Page</div> )
+    }
+}
+
+export default Create
